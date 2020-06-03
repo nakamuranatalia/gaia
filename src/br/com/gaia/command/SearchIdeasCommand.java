a04549d6dcdbee596b180db6f476dca37dcd30c0
Nakamura <nakamura.natalia22@gmail.com>
2020-06-03 14:23:37 -0300

    Creating SearchIdeasCommand class

---
+++ src/br/com/gaia/command/SearchIdeasCommand.java
@@ -0,0 +1,26 @@
+package br.com.gaia.command;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.gaia.model.RecycledMaterial;
+import br.com.gaia.service.RecycledMaterialService;
+
+public class SearchIdeasCommand implements Command{
+
+	private List<RecycledMaterial> recycledMaterialList = new ArrayList<>();
+	private RecycledMaterialService service = new RecycledMaterialService();
+	
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		recycledMaterialList = service.readAll();
+		request.setAttribute("allRecycledMaterials", recycledMaterialList);
+		request.getRequestDispatcher("view/jsp/recycling-ideas/recycling-ideas-search.jsp").forward(request, response);
+	}
+
+}
